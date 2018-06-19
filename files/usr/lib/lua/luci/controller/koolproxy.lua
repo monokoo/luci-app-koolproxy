720d0e6bcf79fe157c6835f42e8ac50e3c613f40
monokoo <realstones2012@gmail.com>
2018-06-19 09:07:59 +0800

    update the order of luci menu

--- files/usr/lib/lua/luci/controller/koolproxy.lua
+++ files/usr/lib/lua/luci/controller/koolproxy.lua
@@ -3,7 +3,7 @@ function index()
 if not nixio.fs.access("/etc/config/koolproxy")then
 return
 end
-entry({"admin","services","koolproxy"},cbi("koolproxy/global"),_("koolproxy"),1).dependent=true
+entry({"admin","services","koolproxy"},cbi("koolproxy/global"),_("koolproxy"),4).dependent=true
 entry({"admin","services","koolproxy","ruleconfig"},cbi("koolproxy/ruleconfig")).leaf=true
 entry({"admin","services","koolproxy","status"},call("act_status")).leaf=true
 end
